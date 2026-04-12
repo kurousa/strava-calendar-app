e76c44b77d38a135b7468567f9e557d1f35b3347
kurousa <r.takahashi.1026@gmail.com>
2026-04-12 22:52:43 +0900

    ci: CIワークフローにプルリクエストへのカバレッジレポート投稿機能を追加しました

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
         coverage: {
             provider: 'v8',
             reporter: ['text', 'json-summary', 'json'],
+            reportOnFailure: true,
             exclude: [
                 'tests/**',
                 'vitest.*.ts',
